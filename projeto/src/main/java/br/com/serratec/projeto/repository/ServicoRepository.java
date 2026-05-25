25c93ed2da5038c938306e5137403823f796f48d
leandrotcdev <leandrotcoutinho@gmail.com>
2026-05-25 16:48:55 -0300

    Adicionando toda a camada de acesso, os arquivos no Repository

---
+++ projeto/src/main/java/br/com/serratec/projeto/repository/ServicoRepository.java
@@ -0,0 +1,8 @@
+package br.com.serratec.projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+}
---
+++ projeto/src/main/java/br/com/serratec/projeto/repository/VeiculoRepository.java
@@ -0,0 +1,12 @@
+package br.com.serratec.projeto.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+    Optional<Veiculo> findByPlaca(String placa);
+
+    List<Veiculo> findByClienteId(Long clienteId);
+}
