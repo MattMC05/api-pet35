8d000769803b622a3e2673c642e3f0c5feaadd64
MattMC05 <matheusm.cogliatti@gmail.com>
2026-05-26 16:24:58 -0300

    alterações endereço e service

--- projeto/src/main/java/br/com/serratec/projeto/repository/VeiculoRepository.java
+++ projeto/src/main/java/br/com/serratec/projeto/repository/VeiculoRepository.java
@@ -3,7 +3,6 @@ package br.com.serratec.projeto.repository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import br.com.serratec.projeto.model.Veiculo;
 
 public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
--- projeto/src/main/java/br/com/serratec/projeto/service/ClienteService.java
+++ projeto/src/main/java/br/com/serratec/projeto/service/ClienteService.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 import br.com.serratec.projeto.configuration.MailConfig;
 import br.com.serratec.projeto.dto.EnderecoResponseDTO;
 import br.com.serratec.projeto.exceptions.ClienteEmailException;
+import br.com.serratec.projeto.exceptions.EnderecoException;
 import br.com.serratec.projeto.model.Cliente;
 import br.com.serratec.projeto.model.Endereco;
 import br.com.serratec.projeto.repository.ClienteRepository;
@@ -31,15 +32,16 @@ public class ClienteService {
         if (clienteBanco != null) {
             throw new ClienteEmailException("Email já cadastrado");
         }
+        buscarCep(cliente.getEndereco().getCep());
 
         mailConfig.sendMail(cliente.getEmail(), "Cadastro de cliente", cliente.toString());
         return clienteRepository.save(cliente);
     }
 
-    /* 
+
+
     private EnderecoResponseDTO buscarCep(String cep){
         Endereco enderecoBanco = enRepository.findByCep(cep);
-
         if (enderecoBanco != null) {
             return new EnderecoResponseDTO(enderecoBanco);
         }else{
@@ -57,6 +59,15 @@ public class ClienteService {
     private EnderecoResponseDTO inserir(Endereco enderecoViaCep) {
         return new EnderecoResponseDTO(enRepository.save(enderecoViaCep));
     }
-    */
+
+
+    public Cliente alterar(Cliente cliente, Long id){
+        if (clienteRepository.existsById(id)) {
+            cliente.setId(id);
+            mailConfig.sendMail(cliente.getEmail(), "Atualização de dados do cliente", cliente.toString());
+            return clienteRepository.save(cliente);
+        }
+        return null;
+    }
 
 }
