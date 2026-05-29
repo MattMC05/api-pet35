7c808f08a55f7091b70ad5ae7a07a5ea3e6aad94
MattMC05 <matheusm.cogliatti@gmail.com>
2026-05-29 14:57:17 -0300

    Parte Individual endereco Finalizada

--- projeto/src/main/java/br/com/serratec/projeto/controller/EnderecoController.java
+++ projeto/src/main/java/br/com/serratec/projeto/controller/EnderecoController.java
@@ -1,13 +1,18 @@
 package br.com.serratec.projeto.controller;
 
+import br.com.serratec.projeto.dto.EnderecoResponseDTO;
 import br.com.serratec.projeto.model.Endereco;
 import br.com.serratec.projeto.service.EnderecoService;
+import jakarta.validation.Valid;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,10 +23,34 @@ public class EnderecoController {
     @Autowired
     private EnderecoService service;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Endereco inserir(@Valid @RequestBody Endereco endereco) {
+        return service.salvar(endereco);
+    }
+
     @GetMapping
+    public ResponseEntity<List<EnderecoResponseDTO>> listar(){
+        return ResponseEntity.ok(service.listarTodos());
+    }
+
+    @GetMapping("/pesquisabairro")
     public ResponseEntity<Page<Endereco>> listarPorBairro(@PageableDefault(sort = "logradouro" ,direction = Direction.ASC)
-        Pageable pageable, @RequestParam(defaultValue = "") String bairro){
+        Pageable pageable, @RequestParam(defaultValue = "Mosela") String bairro){
         return ResponseEntity.ok(service.listarPorBairro(pageable, bairro));
+    }
+
+    @PutMapping("{cep}")
+    public ResponseEntity<Endereco> alterar(@PathVariable String cep, @Valid @RequestBody Endereco endereco) {
+        service.alterar(cep, endereco);
+        var enderecoAtualizado = endereco;
+        return ResponseEntity.ok().body(enderecoAtualizado);
+    }
+
+    @DeleteMapping("{cep}")
+    public ResponseEntity<Void> apagar(@PathVariable String cep){
+        service.apagar(cep);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- projeto/src/main/java/br/com/serratec/projeto/service/EnderecoService.java
+++ projeto/src/main/java/br/com/serratec/projeto/service/EnderecoService.java
@@ -1,10 +1,18 @@
 package br.com.serratec.projeto.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import br.com.serratec.projeto.dto.EnderecoResponseDTO;
+import br.com.serratec.projeto.exceptions.ClienteEmailException;
+import br.com.serratec.projeto.exceptions.EnderecoException;
+import br.com.serratec.projeto.exceptions.ResourceNotFoundException;
 import br.com.serratec.projeto.model.Endereco;
 import br.com.serratec.projeto.repository.EnderecoRepository;
 
@@ -14,8 +22,67 @@ public class EnderecoService {
     @Autowired
     private EnderecoRepository enRepository;
 
+    public Endereco salvar(Endereco endereco){
+        return enRepository.save(buscarCep(endereco.getCep()));
+    }
+
+    public List<EnderecoResponseDTO> listarTodos(){
+        return enRepository.findAll().stream()
+        .map(endereco -> new EnderecoResponseDTO(endereco))
+        .collect(Collectors.toList());
+    }
+
     public Page<Endereco> listarPorBairro(Pageable pageable, String bairro){
         return enRepository.findByBairro(pageable, bairro);
+    }
+
+    public Endereco alterar(String cep, Endereco endereco){
+        if (enRepository.existsById(cep)) {
+            Endereco enderecoExistente = enRepository.findById(cep).get();
+            endereco.setCep(cep);
+            if (endereco.getBairro() == null) {
+                endereco.setBairro(enderecoExistente.getBairro());
+            }
+            if (endereco.getLocalidade() == null) {
+                endereco.setLocalidade(enderecoExistente.getLocalidade());
+            }
+            if (endereco.getLogradouro() == null) {
+                endereco.setLogradouro(enderecoExistente.getLogradouro());
+            }
+            if (endereco.getUf() == null) {
+                endereco.setUf(enderecoExistente.getUf());
+            }
+            return enRepository.save(endereco);
+        }
+        throw new ResourceNotFoundException("Endereco não encontrado");
+    }
+
+    public void apagar(String cep){
+        if (enRepository.existsById(cep)) {
+            enRepository.deleteById(cep);
+        }else{
+            throw new ResourceNotFoundException("Endereco não encontrado");
+        }
+    }
+
+
+
+
+
+    private Endereco buscarCep(String cep){
+        Endereco enderecoBanco = enRepository.findByCep(cep);
+        if (enderecoBanco != null) {
+            throw new ClienteEmailException("Este Cep já está cadastrado");
+        }else{
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "https://viacep.com.br/ws/"+cep+"/json/";
+            Endereco enderecoViaCep = restTemplate.getForObject(url, Endereco.class);
+            if (enderecoViaCep != null) {
+                enderecoViaCep.setCep(enderecoViaCep.getCep().replaceAll("-", ""));
+                return enderecoViaCep;
+            }
+        }
+        throw new EnderecoException("Cep não encontrado!");
     }
 
 }
